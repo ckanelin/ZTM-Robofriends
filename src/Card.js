a852fa86e2373e695163ec21df5234e6a219841d
Candy Lin <lincandy.yite@gmail.com>
2019-10-16 16:57:46 -0700

    Create card template

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () => {
+	return(
+		<div>
+			<img src={'https://robohash.org/0?200x200'} alt='...'></img>
+			<div>
+				<h2>Candy Lin</h2>
+				<p>lincandy.yite@gmail.com</p>
+			</div>
+		</div>
+	);
+}
+
+export default Card;
+
