7ba784c38697951f57d6798e6d0a49f13f579410
Candy Lin <lincandy.yite@gmail.com>
2019-10-16 17:10:03 -0700

    Stylize card

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import 'tachyons';
 
 const Card = () => {
 	return(
-		<div>
+		<div className='tc bg-light-pink washed-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
 			<img src={'https://robohash.org/0?200x200'} alt='...'></img>
 			<div>
 				<h2>Candy Lin</h2>
